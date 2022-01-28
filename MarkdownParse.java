a6014c53e97d8f70f13fca457cd6ba8e6e721731
aajc <ajarrold@ucsd.edu>
2022-01-28 13:17:00 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -14,18 +14,9 @@ public class MarkdownParse {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
 
-            if (markdown.indexOf("(", nextCloseBracket) != nextCloseBracket + 1) {
-                return toReturn;
-            }
-
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-
-            if (openParen == -1) {
-                return toReturn;
-            }
-
-
+            
             toReturn.add(markdown.substring(openParen + 1, closeParen));
 
             currentIndex = closeParen + 1;
